a691f76e7a58ef7a9cb1eaf82196920ad77bee78
Edet Asuquo <eddy1759@gmail.com>
2023-11-21 20:30:35 +0100

    feat: middlewares added

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response, NextFunction } from 'express';
+import jwt, { TokenExpiredError, JwtPayload } from 'jsonwebtoken';
+import httpStatus from 'http-status';
+import config from '../config/index';
+import { User, IUser } from '../models/index';
+import ApiError from '../utils/ApiError';
+
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
+
+const extractToken = (req: Request): string | null => {
+  const bearerToken = req.header('Authorization');
+  return bearerToken && bearerToken.startsWith('Bearer ')
+    ? bearerToken.slice(7, bearerToken.length)
+    : null;
+};
+
+/**
+ * @description Middleware to authenticate user with token 
+ * @param {string} req - Request object 
+ * @param {string} res - Response object
+ * @param {string} next - Next function
+ * @returns {object} - Returns user object 
+ */
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = extractToken(req);
+
+  if (!token) {
+    return next(new ApiError(httpStatus.UNAUTHORIZED, 'Authentication failed'));
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.jwt.secret) as JwtPayload & { sub: string };
+    const user: IUser | null = await User.findById(decoded.sub);
+
+    if (!user) {
+      return next(new ApiError(httpStatus.UNAUTHORIZED, 'User not found'));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      return next(new ApiError(httpStatus.UNAUTHORIZED, 'Token has expired'));
+    }
+    return next(new ApiError(httpStatus.UNAUTHORIZED, 'Invalid token'));
+  }
+};
+
+export default authMiddleware;
---
+++ src/middlewares/validate.ts
@@ -0,0 +1,62 @@
+import Joi from 'joi';
+import express from 'express';
+import httpStatus from 'http-status';
+import pick from '../utils/pick';
+import ApiError from '../utils/ApiError';
+import { logger } from '../config/logger';
+
+type SchemaObject = {
+    params?: Joi.ObjectSchema;
+    query?: Joi.ObjectSchema;
+    body?: Joi.ObjectSchema;
+};
+
+const schemaFields = ['params', 'query', 'body', 'file', 'files'];
+
+export default function validate(schema: SchemaObject) {
+    return (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+    ) => {
+        const validSchema = pick(schema, ['params', 'query', 'body']);
+        const object = pick(req, Object.keys(validSchema));
+        const { value, error } = Joi.compile(validSchema)
+            .prefs({ errors: { label: 'key' }, abortEarly: false })
+            .validate(object);
+        
+        if (error) {
+            const body = {} as Record<string, any>;
+            const query = {} as Record<string, any>;
+            const errorMessage = {} as Record<string, any>;
+            error.details.map((details) => {
+                switch (details.path[0]) {
+                    case 'body':
+                        body[details.path[1]] = details.message.replace(/"/g, "'");
+                        break;
+                    case 'query':
+                        query[details.path[1]] = details.message.replace(/"/g, "'");
+                        break;
+                    default:
+                        break;
+                }
+                logger.error(details);
+            });
+
+            Object.keys(body).length !== 0 && (errorMessage.body = body);
+            Object.keys(query).length !== 0 && (errorMessage.query = query);
+
+            return next(
+                new ApiError(
+                    httpStatus.BAD_REQUEST,
+                    JSON.stringify(errorMessage),
+                    undefined,
+                    undefined,
+                    true,
+                ),
+            );
+        }
+        Object.assign(req, value);
+        return next();
+    };
+}
