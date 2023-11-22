25913debdd163e87aa571f96765c8605091ada16
Oluwatomilola Adeniran <84162702+tun-ji@users.noreply.github.com>
2023-11-22 18:42:49 +0100

    feat: added routes to swagger

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,25 +1,8 @@
-// import swaggerJsdoc from 'swagger-jsdoc'
-// import swaggerUi from 'swagger-ui-express'
-const swaggerAutogen = require('swagger-autogen')();
-// import { Express } from 'express'
-// const options = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'Klusterthon API',
-//       description: "API endpoints for Klusterthon API",
-//       version: '1.0.0',
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:3001/",
-//         description: "Local server"
-//       }
-//     ]
-//   },
-//   // looks for configuration in specified directories
-//   // apis: ['./controllers/*.js'],
-// }
+
+import swaggerDocument from './swagger-output.json';
+import swaggerAutogen from 'swagger-autogen'
+import SwaggerUi from 'swagger-ui-express'
+
 
 const doc = {
 	info: {
@@ -30,6 +13,10 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['../app.ts'];
-// const swaggerSpec = swaggerJsdoc(options)
-swaggerAutogen(outputFile, routes, doc);
+const routes = ['../routes/auth.route.ts', '../routes/user.route.ts'];
+
+
+swaggerAutogen(outputFile, routes, doc)
+
+export default swaggerDocument
+
