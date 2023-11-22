e0b52bc244939dcd4e8070ff1a5493eaf9f40163
Oluwatomilola Adeniran <84162702+tun-ji@users.noreply.github.com>
2023-11-22 17:17:56 +0100

    chore: formatting change

--- src/models/invoice.model.ts
+++ src/models/invoice.model.ts
@@ -4,26 +4,27 @@ import { logger } from '../config/logger';
 import { invoiceStatusTypes } from '../config/invoiceStatusType';
 
 export interface IInvoice extends Document {
-    clientId:  mongoose.Types.ObjectId;
+	clientId: mongoose.Types.ObjectId;
 	amount: Number;
 	dueDate: Date;
-    isPaid: boolean;
-    status: string;
-    createdAt: Date;
-    paymentAttempts: Number;
+	isPaid: boolean;
+	status: string;
+	createdAt: Date;
+	paymentAttempts: Number;
 }
 
 export interface CreateInvoiceBody {
-    clientId:  mongoose.Types.ObjectId;
+	clientId: mongoose.Types.ObjectId;
 	amount: Number;
 	dueDate: Date;
-    isPaid: boolean;
-    status: string;
-    createdAt: Date;
-    paymentAttempts: Number;
+	isPaid: boolean;
+	status: string;
+	createdAt: Date;
+	paymentAttempts: Number;
 }
 
-const invoiceSchema = new Schema<IInvoice>({
+const invoiceSchema = new Schema<IInvoice>(
+	{
 		clientId: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'Client',
@@ -31,31 +32,31 @@ const invoiceSchema = new Schema<IInvoice>({
 		},
 		amount: {
 			type: Number,
-            required: true
+			required: true,
 		},
-        dueDate: {
-            type: Date
-        },
-        isPaid: {
-            type: Boolean,
-            default: false
-        },
-        status: {
-            type: String,
-            enum: [
-                invoiceStatusTypes.OVERDUE,
-                invoiceStatusTypes.PAID,
-                invoiceStatusTypes.PENDING
-            ]
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now()
-        },
-        paymentAttempts: {
-            type: Number,
-            default: 0
-        }
+		dueDate: {
+			type: Date,
+		},
+		isPaid: {
+			type: Boolean,
+			default: false,
+		},
+		status: {
+			type: String,
+			enum: [
+				invoiceStatusTypes.OVERDUE,
+				invoiceStatusTypes.PAID,
+				invoiceStatusTypes.PENDING,
+			],
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+		},
+		paymentAttempts: {
+			type: Number,
+			default: 0,
+		},
 	},
 	{ timestamps: true, collection: 'user' },
 );
